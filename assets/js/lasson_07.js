239851a8e1cee1a648c971eb8e74587a073f2623
antontravkin <travkin.rh@gmail.comu>
2022-02-06 13:07:58 +0300

    for

--- assets/js/lasson_07.js
+++ assets/js/lasson_07.js
@@ -1,9 +1,15 @@
 console.log("");
-let count = 1;
+/* let count = 1;
 do {
     console.log("do");
     count++;
 } while (count <= 10);
 {
     console.log("while");
+} */
+
+let arr = [1, 4, 2, 7, 6, 54, 56, 4, 3, 4];
+
+for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
 }
